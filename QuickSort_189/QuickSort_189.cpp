9fa67216c96c2802ed1fdaca4426d923552b8bc8
salss33 <hallosalsa03@gmail.com>
2023-03-31 15:50:37 +0700

    Menjalankan Input dan Output

--- QuickSort_189/QuickSort_189.cpp
+++ QuickSort_189/QuickSort_189.cpp
@@ -6,3 +6,15 @@ int arr[20];
 int cmp_count = 0; // number of comparison
 int mov_count = 0; // number of movement
 int n;
+void input() {
+	while (true)
+	{
+		cout << "masukan panjang element array :";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\n Maksimum panjang array adalah 20" << endl;
+	
+}
