5d59a418d6a5dc530b1cbd13bbc368895f3e1eaa
salss33 <hallosalsa03@gmail.com>
2023-03-31 15:43:47 +0700

    array of intergrers to hold values

---
+++ QuickSort_189/QuickSort_189.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+// array of integrers to hold values
+int arr[20];
+int cmp_count = 0; // number of comparison
+int mov_count = 0; // number of movement
+int n;
